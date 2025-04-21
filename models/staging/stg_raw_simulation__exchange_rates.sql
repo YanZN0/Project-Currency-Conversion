e7659d85c2057ae7b630f2bbdab7d1c03631f0ab
Yan Robim <yanrobim@gmail.com>
2025-04-21 09:46:26 -0300

    alterações na camada staging

--- models/staging/stg_raw_simulation__exchange_rates.sql
+++ models/staging/stg_raw_simulation__exchange_rates.sql
@@ -1,10 +1,10 @@
 WITH columns_renamed AS (
     SELECT
-        sales_id                    AS sale_id,
+
         original_total_value,
         currency,
-        total_value_usd,
-        conversion_date
+        total_value_USD,
+        data_conversion
 
     FROM {{ source('RAW_SIMULATION', 'EXCHANGE_RATES') }}
 
