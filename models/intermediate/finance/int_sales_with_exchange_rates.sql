8d8b51b3b991da13e105425dd406801ea228d502
Yan Robim <yanrobim@gmail.com>
2025-04-25 11:18:09 -0300

    Melhorando estrutura da tabela intermediaria

--- models/intermediate/finance/int_sales_with_exchange_rates.sql
+++ models/intermediate/finance/int_sales_with_exchange_rates.sql
@@ -12,16 +12,20 @@ WITH applying_exchange_rates AS (
 
         s.sale_id,
         s.sale_date,
+        s.customer_id,
+        s.product_id,
+        s.store_id,
+        s.quantity_ordered,
         ROUND(sale_total_value, 2) AS original_total_value,
         s.currency,
         CASE
-            WHEN s.currency = 'BRL' THEN ROUND(s.sale_total_value * 5.81, 2)
-            WHEN s.currency = 'MXN' THEN ROUND(s.sale_total_value * 19.71, 2)
-            WHEN s.currency = 'EUR' THEN ROUND(s.sale_total_value * 0.87, 2)
+            WHEN s.currency = 'BRL' THEN ROUND(s.sale_total_value * 5.68, 2)
+            WHEN s.currency = 'MXN' THEN ROUND(s.sale_total_value * 19.63, 2)
+            WHEN s.currency = 'EUR' THEN ROUND(s.sale_total_value * 0.88, 2)
             WHEN s.currency = 'USD' THEN s.sale_total_value
         END AS total_value_usd,
 
-        CURRENT_DATE AS date_conversion
+        CURRENT_DATE AS rate_date
 
     FROM {{ ref('stg_raw_simulation__sales') }} s
     LEFT JOIN {{ ref('stg_raw_simulation__exchange_rates') }} e
