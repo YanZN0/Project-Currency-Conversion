0ab54c74773f704c86d10928086a785f058729a5
Yan Robim <yanrobim@gmail.com>
2025-04-28 07:41:08 -0300

    Métricas de clientes conclúidas + refatoramento de código sqlfluff

--- models/intermediate/sales/int__customers_metrics.sql
+++ models/intermediate/sales/int__customers_metrics.sql
@@ -1,22 +1,39 @@
-with source as (
-    select *
-    from {{ ref('stg_raw_simulation__customers') }}
+{{
+    config(
+      materialized = 'incremental',
+      unique_key = 'customer_id',
+      incremental_strategy = 'merge'
+    )
+}}
+
+
+WITH source AS (
+    SELECT *
+    FROM {{ ref('stg_raw_simulation__customers') }}
 
 ),
 
-
-metrics as (
-    select
+metrics AS (
+    SELECT
         c.customer_id,
         c.customer_name,
-        COUNT(s.sale_id) as total_customer_orders,
-        ROUND(SUM(s.total_value_usd), 2) as total_spent,
-        MIN(s.sale_date) as first_purchase,
-        MAX(s.sale_date) as last_purchase,
-        c.customer_vip
-    from source c
-    left join {{ ref('int__sales_with_exchange_rates') }} s ON c.customer_id = s.customer_id
-    group by c.customer_id, c.customer_name, c.customer_vip
+        c.customer_vip,
+        COUNT(s.sale_id) AS total_customer_orders,
+        SUM(s.total_value_usd) AS total_spent,
+        MIN(s.sale_date) AS first_purchase,
+        MAX(s.sale_date) AS last_purchase
+    FROM source c
+    LEFT JOIN {{ ref('int__sales_with_exchange_rates') }} s ON c.customer_id = s.customer_id
+    WHERE s.sale_date IS NOT NULL
+    GROUP BY c.customer_id, c.customer_name, c.customer_vip
 )
 
-select * from metrics
+SELECT * FROM metrics
+
+{% if is_incremental() %}
+
+    where last_purchase > (select max(last_purchase)
+    from {{ this }}
+    where customer_id = metrics.customer_id
+  )
+{% endif %}
