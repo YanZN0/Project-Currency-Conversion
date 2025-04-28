76310e047927751fd9da728cad658f9cd39a6325
Yan Robim <yanrobim@gmail.com>
2025-04-28 10:27:21 -0300

    Derivando métricas em nível de produtos + Refatoração código com sqlfluff

---
+++ models/intermediate/sales/int__products_metrics.sql
@@ -0,0 +1,45 @@
+{{
+    config(
+      materialized = 'incremental',
+      unique_key = 'product_id',
+      incremental_strategy = 'merge'
+    )
+}}
+
+WITH source AS (
+    SELECT *
+    FROM {{ ref('stg_raw_simulation__products') }}
+
+),
+
+metrics AS (
+
+    SELECT
+        p.product_id,
+        p.product_price,
+        p.product_category,
+        COUNT(s.sale_id) AS total_sales_count,
+        SUM(total_value_usd) AS total_revenue_usd,
+        ROUND(total_revenue_usd / total_sales_count, 2) AS average_sale_value_usd,
+        MIN(s.sale_date) AS first_sale_product,
+        MAX(s.sale_date) AS last_sale_product
+
+    FROM source p
+    LEFT JOIN {{ ref('int__sales_with_exchange_rates') }} s ON p.product_id = s.product_id
+    GROUP BY
+        p.product_id,
+        p.product_price,
+        p.product_category
+
+)
+
+SELECT * FROM metrics
+
+{% if is_incremental() %}
+
+    WHERE last_purchase > (
+        SELECT MAX(last_sale_product)
+        FROM {{ this }}
+        WHERE product_id = metrics.product_id
+    )
+{% endif %}
