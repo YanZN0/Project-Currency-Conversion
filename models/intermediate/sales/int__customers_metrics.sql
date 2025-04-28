a4419e1ee1558b1a02c06454dda40c851635c9a9
Yan Robim <yanrobim@gmail.com>
2025-04-28 10:27:58 -0300

    Refatoração de código sqlfluff

--- models/intermediate/sales/int__customers_metrics.sql
+++ models/intermediate/sales/int__customers_metrics.sql
@@ -19,7 +19,7 @@ metrics AS (
         c.customer_name,
         c.customer_vip,
         COUNT(s.sale_id) AS total_customer_orders,
-        SUM(s.total_value_usd) AS total_spent,
+        SUM(s.total_value_usd) AS total_spend,
         MIN(s.sale_date) AS first_purchase,
         MAX(s.sale_date) AS last_purchase
     FROM source c
@@ -32,8 +32,9 @@ SELECT * FROM metrics
 
 {% if is_incremental() %}
 
-    where last_purchase > (select max(last_purchase)
-    from {{ this }}
-    where customer_id = metrics.customer_id
-  )
+    WHERE last_purchase > (
+        SELECT MAX(last_purchase)
+        FROM {{ this }}
+        WHERE customer_id = metrics.customer_id
+    )
 {% endif %}
