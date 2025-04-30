0f19d7f2204c5f86a85d894d74ff424fd49555d5
Yan Robim <yanrobim@gmail.com>
2025-04-30 11:12:53 -0300

    Inicialização da camada Marts, organização de Fatos e Dimensões

---
+++ models/marts/analytics/dim__exchange_rates.sql
@@ -0,0 +1,24 @@
+with exchange_rates as (
+
+    select *
+    from {{ ref('int__sales_with_exchange_rates') }}
+
+),
+
+exchange_rates_separation as (
+
+    select
+
+        currency as of_currency,
+
+        'USD' as to_currency,
+
+        ROUND(original_total_value / total_value_usd, 2) as exchange_rate_used,
+
+        CURRENT_DATE as exchange_rate_date
+
+    from exchange_rates
+
+)
+
+select * from exchange_rates_separation
---
+++ models/marts/analytics/fct__sales.sql
@@ -0,0 +1,24 @@
+with sales as (
+    select *
+    from {{ ref('int__sales_with_exchange_rates') }}
+),
+
+
+sales_with_metrics_usd as (
+    select
+            sale_id,
+            sale_date,
+            customer_id,
+            product_id,
+            store_id,
+            product_unit_price,
+            quantity_ordered,
+            original_total_value,
+            currency as of_currency,
+            total_value_usd,
+            conversion_date
+
+    from sales
+)
+
+select * from sales_with_metrics_usd
