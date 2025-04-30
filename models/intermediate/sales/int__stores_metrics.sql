b4514256491f0bf4c5851655beb99ff5a0ae5f86
Yan Robim <yanrobim@gmail.com>
2025-04-30 08:41:25 -0300

    Métricas avaliativas das lojas concluidas + Refatoração com sqlfluff

---
+++ models/intermediate/sales/int__stores_metrics.sql
@@ -0,0 +1,47 @@
+{{
+    config(
+      materialized = 'incremental',
+      unique_key = 'store_id',
+      incremental_strategy = 'merge'
+    )
+}}
+
+
+WITH source AS (
+    SELECT *
+    FROM {{ ref('stg_raw_simulation__stores') }}
+),
+
+metrics AS (
+
+    SELECT
+        s.store_id,
+        s.store_name,
+        s.store_city,
+        s.state,
+        COUNT(e.sale_id) AS total_sales_store,
+        SUM(e.total_value_usd) AS total_revenue_store_usd,
+        ROUND(total_revenue_store_usd / total_sales_store, 2) AS average_sale_value_usd,
+        MIN(e.sale_date) AS first_sale,
+        MAX(e.sale_date) AS last_sale
+
+    FROM source s
+    LEFT JOIN {{ ref('int__sales_with_exchange_rates') }} e ON s.store_id = e.store_id
+    GROUP BY
+        s.store_id,
+        s.store_name,
+        s.store_city,
+        s.state
+)
+
+SELECT * FROM metrics
+
+
+{% if is_incremental() %}
+
+    WHERE last_purchase > (
+        SELECT MAX(last_sale_product)
+        FROM {{ this }}
+        WHERE store_id = metrics.store_id
+    )
+{% endif %}
